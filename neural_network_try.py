d8952a65dbf9b319b713a6623180c05b561ce76c
Ole Vegard Mythe Moland <ole-vegard.moland@telenordigital.com>
2017-12-14 17:05:12 +0100

    A better ( hopefully ) try

--- neural_network_try.py
+++ neural_network_try.py
@@ -5,15 +5,10 @@ import board_utils
 import board_printer
 
 
-# Board
-board = [
-    2, 8, # x0, x1
-    2, 4] # x2 x3
 
 
 verbose = False
 
-print("Score before : " + str(board_utils.get_score(board)))
 
 def do_print(str):
     if verbose:
@@ -28,7 +23,7 @@ def sigmoid(x, deriv=False):
 
 class Node:
     def __init__(self):
-        self.weight = 0.5
+        self.weight = 0.5 * get_random()
 
     def update(self, input_1, input_2):
         if input_1 == input_2:
@@ -36,47 +31,106 @@ class Node:
         else:
             self.val = abs(1 / abs((input_1 - input_2)))
 
-        do_print("Inputs : {}, {} Res : {}".format(input_1, input_2, self.val))
+        # do_print("Inputs : {}, {} Res : {}".format(input_1, input_2, self.val))
 
     def calculate(self):
         result = self.weight * self.val
         sigm = sigmoid(result) if self.val != 0 else 1
-        do_print("Value : {:6.4f} Weighted value : {:6.4f} sigmoid {:6.4f}".format(self.val, result, sigm) )
+        # do_print("Value : {:6.4f} Weighted value : {:6.4f} sigmoid {:6.4f}".format(self.val, result, sigm) )
 
         return sigm
 
 
-def get_nodes():
+def get_nodes(board):
     nodes = []
 
     for i in range(len(board) - 1):
         j = i + 1
         while j < len(board):
-            print("Adding node {}. {}".format(i, j))
+            do_print("Adding node {}. {}".format(i, j))
+
             n = Node()
             n.update(board[i], board[j])
+
             nodes.append(n)
             j += 1
 
     return nodes
 
-nodes = get_nodes()
+np.random.seed(0)
 
-left = nodes[0].calculate() + nodes[5].calculate()
-down = nodes[1].calculate() + nodes[4].calculate()
+def get_random():
+    return sigmoid(np.random.random(1)[0], deriv=False) + 0.4
 
-print("Left : " + str(left))
-print("Down : " + str(down))
-
-print(board_printer.print_board(board))
-
-if left > down:
-    board = board_mover.move_all_left(board)
-else:
-    board = board_mover.move_all_down(board)
+def adjust_weights(nodes, increase):
+    for node in nodes:
+        if increase:
+            node.weight *= max(get_random(), 1.0)
+        else:
+            node.weight *= min(get_random(), 1.0)
 
 
-print("Score after : " + str(board_utils.get_score(board)))
-print(board_printer.print_board(board))
+def evaulate_nodes(nodes_left, nodes_down, board):
+    left = sum([node.calculate() for node in nodes_left])
+    down = sum([node.calculate() for node in nodes_down])
+
+    do_print("Sum left : " + str(left))
+    do_print("Sum down : " + str(down))
+
+    if left > down:
+        do_print("Move down")
+        board = board_mover.move_all_left(board)
+    elif left < down:
+        do_print("Move up")
+        board = board_mover.move_all_down(board)
+    else:
+        do_print("No change!")
+
+    return board
 
 
+def print_board_and_score(board):
+    board_printer.print_board(board)
+    print("Score : " + str(board_utils.get_score(board)))
+
+def train(board, left_should_win = False, down_should_win = False):
+    if not left_should_win and not down_should_win:
+        print("Either left_win or down_win should be True")
+        return
+    board = board.copy()
+
+    score_before = board_utils.get_score(board)
+
+    nodes_left = get_nodes(board)
+    nodes_down = get_nodes(board)
+
+    print(" Before ".center(80, "="))
+    print_board_and_score(board)
+    board = evaulate_nodes(nodes_left, nodes_down, board)
+
+    print(" After ".center(80, "="))
+    score_after = board_utils.get_score(board)
+    print_board_and_score(board)
+
+
+    if score_after == score_before:
+        print(" Score is the same ".center(80, "="))
+        if left_should_win:
+            adjust_weights(nodes_left, True)
+            adjust_weights(nodes_down, False)
+        elif down_should_win:
+            adjust_weights(nodes_left, True)
+            adjust_weights(nodes_down, False)
+        print("=" * 80)
+        return False
+    else:
+        print(" Score is higher ".center(80, "="))
+        return True
+
+board = [
+    2, 8, 2, # x0, x1
+    4, 8, 4, # x0, x1
+    2, 4, 2] # x2 x3
+
+train(board, left_should_win=False, down_should_win=True)
+train(board, left_should_win=False, down_should_win=True)
